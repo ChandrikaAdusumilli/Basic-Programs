2253f33879ab3c2964236cfe956008e1def326a2
Chandrika Adusumilli <adusumillichandrika@gmail.com>
2025-05-30 09:56:56 +0530

    Update AdditionOfTwoNumbers.java

--- AdditionOfTwoNumbers.java
+++ AdditionOfTwoNumbers.java
@@ -1,10 +1,10 @@
 import java.util.Scanner;
 public class AdditionOfTwoNumbers{
-public static void main(String args[]){
-Scanner sc=new Scanner(System.in);
-int a=sc.nextInt();
-int b=sc.nextInt();
-int result=a+b;
-System.out.println(result);
-}
+ public static void main(String args[]){
+   Scanner sc=new Scanner(System.in);
+   int a=sc.nextInt();
+   int b=sc.nextInt();
+   int result=a+b;
+  System.out.println(result);
+  }
 }
