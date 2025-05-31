de635c2d461be64c262d042c6bb328456081bd09
Chandrika Adusumilli <adusumillichandrika@gmail.com>
2025-05-31 21:33:26 +0530

    Add files via upload

---
+++ PalindromesUptoN.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+public class PalindromesUptoN{
+public static boolean isPalindrome(int n){
+int rem=0,rev=0,originalnumber=n;
+while(n>0){
+rem=n%10;
+rev=rev*10+rem;
+n/=10;
+}
+if(originalnumber==rev)
+return true;
+else
+ return false;
+ }
+public static void main(String args[]){
+Scanner sc=new Scanner(System.in);
+int n=sc.nextInt();
+for(int i=0i<=n;i++){
+if(isPalindrome(i){
+System.out.println(i);
+}
+}
+}
+}
